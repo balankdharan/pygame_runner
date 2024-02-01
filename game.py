40ffffee582c57dbb513b0372d96b7196145bdea
balankdharan <balankdharan@gmail.com>
2024-02-01 19:05:30 +0530

    General setup for game

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen=pygame.display.set_mode((800,400))
+pygame.display.set_caption("Runner")
+clock=pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #draw all our elements
+    #update everything
+    pygame.display.update()
+    clock.tick(60)
