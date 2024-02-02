2291322b8225c303ca8ec19bae570308346c694a
balankdharan <balankdharan@gmail.com>
2024-02-02 17:03:44 +0530

    setting up images and fonts

--- game.py
+++ game.py
@@ -5,7 +5,12 @@ pygame.init()
 screen=pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock=pygame.time.Clock()
+test_font=pygame.font.Font('font/Pixeltype.ttf',50)
 
+
+sky_surface=pygame.image.load('graphics/Sky.png')
+ground_surface=pygame.image.load('graphics/ground.png')
+test_surface=test_font.render("Strat the game",False,'Black')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,6 +18,9 @@ while True:
             exit()
 
     #draw all our elements
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(test_surface,(300,50))
     #update everything
     pygame.display.update()
     clock.tick(60)
