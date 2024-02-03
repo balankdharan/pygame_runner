2931b998ea4114a33d280f94bec94fc40231f224
balankdharan <balankdharan@gmail.com>
2024-02-03 12:34:43 +0530

    added rectangle to the surface for the position

--- game.py
+++ game.py
@@ -8,9 +8,16 @@ clock=pygame.time.Clock()
 test_font=pygame.font.Font('font/Pixeltype.ttf',50)
 
 
-sky_surface=pygame.image.load('graphics/Sky.png')
-ground_surface=pygame.image.load('graphics/ground.png')
+sky_surface=pygame.image.load('graphics/Sky.png').convert()
+ground_surface=pygame.image.load('graphics/ground.png').convert()
 test_surface=test_font.render("Strat the game",False,'Black')
+
+snail_surf=pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect=snail_surf.get_rect(bottomright=(600,300))
+
+player_surf=pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect=player_surf.get_rect(midbottom=(80,300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,6 +28,12 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(test_surface,(300,50))
+
+    if snail_rect.right <= 0 :snail_rect.left=800
+    snail_rect.x -=4
+
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf,player_rect)
     #update everything
     pygame.display.update()
     clock.tick(60)
